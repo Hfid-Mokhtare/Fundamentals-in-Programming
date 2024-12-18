4cc3dea1ad9118bf9b1d81f935da0c490f9463c9
Mohamed Ali + <137907946+Hfid-Mokhtare@users.noreply.github.com>
2024-12-18 10:30:45 +0100

    Update Problem #009 : Digit Frequency2.cpp

--- 005_Algorithms and Problem Solving Level 2/Problem #009 : Digit Frequency2.cpp
+++ 005_Algorithms and Problem Solving Level 2/Problem #009 : Digit Frequency2.cpp
@@ -1,3 +1,89 @@
+// after one year i came backe to solve this problem and i do it butter than my teacher
+// the speed of my programme is : 3515881  microseconds
+// the speed of my teacher programme is : 4745802 microseconds
+
+//Me : 
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int ReadPositiveNumber(string Message)
+{
+	int Number = 0;
+	do
+	{
+		cout << Message << endl;
+		cin >> Number;
+
+	} while (Number <= 0);
+
+	return Number;
+}
+
+int GitDigit(int Number)
+{
+	return Number % 10;
+}
+
+int Counte_Digit(int Num, short Digit)
+{
+	int counter = 0;
+	while (Num> 0)
+	{
+		
+		if (Digit == GitDigit(Num))
+		{
+			counter++;
+		}
+		
+		Num = Num /= 10;
+		
+	}
+	return counter;
+}
+
+int Delet_Digits(int Num, short Digit)
+{
+	int Num2 = 0;
+	short Remainder = 0;
+	while (Num > 0)
+	{
+		Remainder = Num % 10;
+		if (Remainder != Digit)
+		{
+			Num2 = Num2 * 10 + Remainder;
+		}
+		Num = Num / 10;
+	}
+	return Num2;
+}
+
+void Counte_Digits_Frecuancy(int Num)
+{
+	short Digit_To_cownte = 0;
+
+	while(Num>0)
+	{
+		Digit_To_cownte = Num % 10;
+		
+		cout << "Digit " << Digit_To_cownte << " Frequency is " 
+			<< Counte_Digit(Num, Digit_To_cownte) << " Time(s)" << endl;
+
+		Num = Delet_Digits(Num, Digit_To_cownte);
+
+	}
+}
+
+int main()
+{
+	int Num = ReadPositiveNumber("Please Enter a Positive Number : ");
+	Counte_Digits_Frecuancy(Num);
+	
+	return 0;
+}
+ 
+===============================================================================
 //Me : 
 #include <iostream>
 #include <string>
