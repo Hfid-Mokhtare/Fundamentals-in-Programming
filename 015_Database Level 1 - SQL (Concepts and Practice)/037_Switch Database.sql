3ca3ac45b2f14695c9db4507f7bb2ce55523aafb
Mohamed Ali + <137907946+Hfid-Mokhtare@users.noreply.github.com>
2025-04-02 21:47:36 +0000

    Update and rename 037_Switch Database.txt to 037_Switch Database.sql

---
+++ 015_Database Level 1 - SQL (Concepts and Practice)/037_Switch Database.sql
@@ -0,0 +1,7 @@
+-- to switch databade you can use the comande : 
+
+USE koko;
+
+-- all querys that you write will execute on the database selected; if not it will run on the master database 
+
+-- or you can use the mouse and select the database name 
