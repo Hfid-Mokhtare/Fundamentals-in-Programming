df68ce80196c91b91cc1bf540971c2b0acd19de5
Mohamed Ali + <137907946+Hfid-Mokhtare@users.noreply.github.com>
2025-04-02 21:48:30 +0000

    Update and rename 036_Create Database IF NOT EXISTS.txt to 036_Create Database IF NOT EXISTS.sql

--- 015_Database Level 1 - SQL (Concepts and Practice)/036_Create Database IF NOT EXISTS.sql
+++ 015_Database Level 1 - SQL (Concepts and Practice)/036_Create Database IF NOT EXISTS.sql
@@ -1,13 +1,12 @@
-- to do comantes in sql : 
-  use : --
+-- to do comantes in sql use : --
 
-- to create database if not exists : 
+-- to create database if not exists : 
 
   if not exists(select * from sys.databases where name='koko4')
   begin
 	  create database koko4;
   end
 
-- in MySql : 
+-- in MySql : 
 
   create databade if not exists koko4;
