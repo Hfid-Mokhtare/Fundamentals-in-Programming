9f07f10c164ae056571ab3554224865afe61c1cc
Mohamed Ali + <137907946+Hfid-Mokhtare@users.noreply.github.com>
2025-04-02 23:28:52 +0000

    Update 040_Create Table.sql

--- 015_Database Level 1 - SQL (Concepts and Practice)/040_Create Table.sql
+++ 015_Database Level 1 - SQL (Concepts and Practice)/040_Create Table.sql
@@ -1,1 +1,17 @@
+-- to create a table you can use the mouse 
+-- or you can use the comands : 
 
+create table employes(
+	ID int not null,
+	name nvarchar(50) not null,
+	phone nvarchar(10) null,
+	salery smallmoney null,
+	primary key(ID)
+);
+
+-- in MySql : 
+CREATE TABLE users (
+    user_id INT PRIMARY KEY,
+    username VARCHAR(50),
+    email VARCHAR(100)
+);
