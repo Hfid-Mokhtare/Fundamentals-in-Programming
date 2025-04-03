b72c848a5d4cc16ad4863979aaee6a9b738ab78f
Mohamed Ali + <137907946+Hfid-Mokhtare@users.noreply.github.com>
2025-04-03 10:38:06 +0000

    Update 040_Create Table.sql

--- 015_Database Level 1 - SQL (Concepts and Practice)/040_Create Table.sql
+++ 015_Database Level 1 - SQL (Concepts and Practice)/040_Create Table.sql
@@ -1,6 +1,5 @@
 -- to create a table you can use the mouse 
 -- or you can use the comands : 
-
 create table employes(
 	ID int not null,
 	name nvarchar(50) not null,
@@ -9,9 +8,27 @@ create table employes(
 	primary key(ID)
 );
 
+-- to check if not exists befor creating : 
+if not exists(select * from sys.tables where name='EM')
+begin
+	create table EM(
+		ID int primary key not null,
+		name varchar(50) not null,
+		age int null
+	);
+end;
+
+
 -- in MySql : 
 CREATE TABLE users (
     user_id INT PRIMARY KEY,
     username VARCHAR(50),
     email VARCHAR(100)
+);
+
+-- to check if not exists befor creating : 
+CREATE TABLE IF NOT EXISTS EM (
+    ID INT PRIMARY KEY NOT NULL,
+    name VARCHAR(50) NOT NULL,
+    age INT NULL
 );
