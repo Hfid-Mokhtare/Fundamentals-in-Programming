cba52567cd79d6a302178645b7692beff4cf63fc
Mohamed Ali + <137907946+Hfid-Mokhtare@users.noreply.github.com>
2025-04-03 00:41:13 +0000

    Update 046_Modify a column.sql

--- 015_Database Level 1 - SQL (Concepts and Practice)/046_Modify a column.sql
+++ 015_Database Level 1 - SQL (Concepts and Practice)/046_Modify a column.sql
@@ -1,1 +1,8 @@
+-- to modify a column is sql server : 
 
+ALTER TABLE employees
+ALTER COLUMN Gender VARCHAR(100) NULL;
+
+-- in mysql : 
+ALTER TABLE employees
+MODIFY COLUMN Gender VARCHAR(100) NULL;
