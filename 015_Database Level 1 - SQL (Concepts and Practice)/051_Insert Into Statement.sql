59a3f67ef5d0a9eeb6547c1a986556d885e89d01
Mohamed Ali + <137907946+Hfid-Mokhtare@users.noreply.github.com>
2025-04-03 10:08:16 +0000

    Update 051_Insert Into Statement.sql

--- 015_Database Level 1 - SQL (Concepts and Practice)/051_Insert Into Statement.sql
+++ 015_Database Level 1 - SQL (Concepts and Practice)/051_Insert Into Statement.sql
@@ -1,1 +1,15 @@
+-- to insert values in table columns : 
+insert into employes
+values
+(1,'Mohamed Ali','0611273281',1000),
+(2,'Ali','2345678',2000);
 
+-- to insert only some columns :
+-- only the columns that not acception null .
+insert into employes (ID,name) 
+values
+(5,'Yassir'),
+(6,'Reda');
+
+-- to delete the table : 
+drop table employes;
