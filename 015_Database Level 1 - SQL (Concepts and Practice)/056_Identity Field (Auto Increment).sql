589cf554e43fe8a7f7956dc753c1f45526143b87
Mohamed Ali + <137907946+Hfid-Mokhtare@users.noreply.github.com>
2025-04-03 23:42:16 +0000

    Update 056_Identity Field (Auto Increment).sql

--- 015_Database Level 1 - SQL (Concepts and Practice)/056_Identity Field (Auto Increment).sql
+++ 015_Database Level 1 - SQL (Concepts and Practice)/056_Identity Field (Auto Increment).sql
@@ -1,1 +1,16 @@
+-- to creat an column with auto increment
+create table EM1(
+	ID int identity(1,1) not null, -- ( increment , start from )
+	name varchar(200)not null,
+	primary key(ID)
+);
 
+insert into EM1
+values('HR')
+
+
+-- in MySql 
+CREATE TABLE EM1 (
+    ID INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(200) NOT NULL
+);
