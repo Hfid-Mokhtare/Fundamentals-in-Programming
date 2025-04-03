927f0020d19dc2f7a000bca00b3c46133016de0a
Mohamed Ali + <137907946+Hfid-Mokhtare@users.noreply.github.com>
2025-04-03 11:08:22 +0000

    Create 053_Delete Statement.sql

---
+++ 015_Database Level 1 - SQL (Concepts and Practice)/053_Delete Statement.sql
@@ -0,0 +1,10 @@
+-- to delete a recorde from the table 
+delete from EM
+where ID = 102;
+
+-- where the condition is null 
+delete from EM
+where salery is null;
+
+-- to delete all data : 
+delete from EM;
