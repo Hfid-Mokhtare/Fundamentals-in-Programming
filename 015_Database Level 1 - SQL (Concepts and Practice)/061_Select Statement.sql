a84a904bd731622a045a0c7ece7927fc1012e616
Mohamed Ali + <137907946+Hfid-Mokhtare@users.noreply.github.com>
2025-04-04 01:25:56 +0000

    Create 061_Select Statement.sql

---
+++ 015_Database Level 1 - SQL (Concepts and Practice)/061_Select Statement.sql
@@ -0,0 +1,11 @@
+-- how to use SELECT statement : 
+
+use HR_Database;
+
+select * from Employees;
+
+select ID, FirstName, LastName from Employees; -- to chose specific column
+
+select * from Departments;
+
+select * from Countries;
